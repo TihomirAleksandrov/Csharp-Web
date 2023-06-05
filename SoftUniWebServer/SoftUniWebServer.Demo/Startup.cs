a07cde62b302a912f0e99a24fc459f8c860a0747
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-06-05 14:57:07 +0300

    Added new files

---
+++ SoftUniWebServer/SoftUniWebServer.Demo/Startup.cs
@@ -0,0 +1,13 @@
+﻿using SoftUniWebServer.Server;
+
+namespace SoftUniWebServer.Demo
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
