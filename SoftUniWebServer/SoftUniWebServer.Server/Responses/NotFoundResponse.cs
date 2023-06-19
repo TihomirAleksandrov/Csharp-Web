1789820c45bfdca9aacf6c151a0be4050b355eb4
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-06-19 14:00:18 +0300

    Added new features

---
+++ SoftUniWebServer/SoftUniWebServer.Server/Responses/NotFoundResponse.cs
@@ -0,0 +1,17 @@
+﻿using SoftUniWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniWebServer.Server.Responses
+{
+    public class NotFoundResponse : Response
+    {
+        public NotFoundResponse()
+            : base(StatusCode.Notfound)
+        {
+        }
+    }
+}
