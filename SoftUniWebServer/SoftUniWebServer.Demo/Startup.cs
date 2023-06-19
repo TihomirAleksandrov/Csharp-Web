5bea0d77d361885c6a399a534f882df22342f64a
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-06-19 13:59:36 +0300

    Added new features

--- SoftUniWebServer/SoftUniWebServer.Demo/Startup.cs
+++ SoftUniWebServer/SoftUniWebServer.Demo/Startup.cs
@@ -1,13 +1,34 @@
 ﻿using SoftUniWebServer.Server;
+using SoftUniWebServer.Server.HTTP;
+using SoftUniWebServer.Server.Responses;
 
 namespace SoftUniWebServer.Demo
 {
     internal class Startup
     {
+        private const string HtmlForm = @"<form aaction='/HTML' method='POST'>
+Name: <input type='text' name='Name'/>
+Age: <input type='number' name='Age'/>
+<input type='submit' value ='Save' />
+</form>";
+
         static void Main(string[] args)
+            => new HttpServer(routes => routes
+            .MapGet("/", new TextResponse("Hello from the server!"))
+            .MapGet("/Redirect", new Redirectresponse("https://softuni.org/"))
+            .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
+            .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction)))
+        .Start();
+
+        private static void AddFormDataAction(Request request, Response response)
         {
-            var server = new HttpServer("127.0.0.1", 8080);
-            server.Start();
+            response.Body = "";
+
+            foreach (var (key, value) in request.Form)
+            {
+                response.Body += $"{key} - {value}";
+                response.Body += Environment.NewLine;
+            }
         }
     }
 }
