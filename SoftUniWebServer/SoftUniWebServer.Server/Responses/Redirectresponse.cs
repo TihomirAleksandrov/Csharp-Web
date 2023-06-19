8b566346740ae4b4b994760d10e890e37037fcfc
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-06-19 14:00:29 +0300

    Added new features

---
+++ SoftUniWebServer/SoftUniWebServer.Server/Responses/Redirectresponse.cs
@@ -0,0 +1,18 @@
+﻿using SoftUniWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniWebServer.Server.Responses
+{
+    public class Redirectresponse : Response
+    {
+        public Redirectresponse(string location)
+            : base(StatusCode.Found)
+        {
+            this.Headers.Add(Header.Location, location);
+        }
+    }
+}
---
+++ SoftUniWebServer/SoftUniWebServer.Server/RoutingTable.cs
@@ -0,0 +1,67 @@
+﻿using SoftUniWebServer.Server.Common;
+using SoftUniWebServer.Server.HTTP;
+using SoftUniWebServer.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniWebServer.Server
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, Dictionary<string, Response>> _routes;
+
+        public RoutingTable()
+            => _routes = new()
+            {
+                [Method.Get] = new(),
+                [Method.Post] = new(),
+                [Method.Put] = new(),
+                [Method.Delete] = new(),
+            };
+
+
+        public IRoutingTable Map(string url, Method method, Response response)
+            => method switch
+            {
+                Method.Get => this.MapGet(url, response),
+                Method.Post => this.MapPost(url, response),
+                _ => throw new InvalidOperationException($"Method '{method}' is not supported.")
+            };
+
+        public IRoutingTable MapGet(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            _routes[Method.Get][url] = response;
+
+            return this;
+        }
+
+        public IRoutingTable MapPost(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            _routes[Method.Post][url] = response;
+
+            return this;
+        }
+
+        public Response MatchRequest(Request request)
+        {
+            var requestMethod = request.Method;
+            var requestUrl = request.Url;
+
+            if (!_routes.ContainsKey(requestMethod) || !_routes[requestMethod].ContainsKey(requestUrl))
+            {
+                return new NotFoundResponse();
+            }
+
+            return _routes[requestMethod][requestUrl];
+        }
+    }
+}
