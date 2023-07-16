9f3624ad8e0df07b6a63117c7d38b5bb181b10cf
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-07-16 14:31:34 +0300

    Added cookies and session functionality

--- SoftUniWebServer/SoftUniWebServer.Demo/Startup.cs
+++ SoftUniWebServer/SoftUniWebServer.Demo/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using SoftUniWebServer.Server;
 using SoftUniWebServer.Server.HTTP;
 using SoftUniWebServer.Server.Responses;
+using System.Text;
+using System.Web;
 
 namespace SoftUniWebServer.Demo
 {
@@ -18,6 +20,15 @@ Age: <input type='number' name='Age'/>
 
         private const string FileName = "content.txt";
 
+        private const string LoginForm = @"<form action='/Login' method='POST'>
+Username: <input type='text' name='Username'/>
+Password: <input type='text' name='Password'/>
+<input type='submit' value='Log In' />
+</form>";
+
+        private const string Username = "user";
+        private const string Password = "user123";
+
         public static async Task Main()
         {
             await DownloadSitesAsTextFile(Startup.FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
@@ -28,8 +39,122 @@ Age: <input type='number' name='Age'/>
             .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
             .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
             .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
-            .MapPost("/Content", new TextFileResponse(Startup.FileName)));
+            .MapPost("/Content", new TextFileResponse(Startup.FileName))
+            .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction))
+            .MapGet("/Session", new TextResponse("", Startup.DisplaySessionInfoAction))
+            .MapGet("/Login", new HtmlResponse(Startup.LoginForm))
+            .MapPost("/Login", new HtmlResponse("", Startup.LoginAction))
+            .MapGet("/Logout", new HtmlResponse("", Startup.LogoutAction))
+            .MapGet("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)));
             await server.Start();
+        }
+
+        private static void GetUserDataAction(Request request, Response response)
+        {
+            if (request.Session.ContainsKey(Session.SessionUserKey))
+            {
+                response.Body = "";
+                response.Body += $"<h3>Currently logged-in user " + 
+                    $"is with username '{Username}'</h3>";
+            }
+            else
+            {
+                response.Body = "";
+                response.Body += "<h3>You should first log in " +
+                    "- <a href='/Login'>Login</a></h3>";
+            }
+        }
+
+        private static void LogoutAction(Request request, Response response)
+        {
+            request.Session.Clear();
+
+            response.Body = "";
+            response.Body += "<h3>Logged out successfully!</h3>";
+        }
+
+        private static void LoginAction(Request request, Response response)
+        {
+            request.Session.Clear();
+
+            var bodyText = "";
+
+            var usernameMatches = request.Form["Username"] == Startup.Username;
+            var passwordMatches = request.Form["Password"] == Startup.Password;
+
+            if (usernameMatches & passwordMatches)
+            {
+                request.Session[Session.SessionUserKey] = "MyUserId";
+                response.Cookies.Add(Session.SessionCookieName, request.Session.Id);
+
+                bodyText = "<h3>Logged successfully!</h3>";
+            }
+            else
+            {
+                bodyText = Startup.LoginForm;
+            }
+
+            response.Body = "";
+            response.Body += bodyText;
+        }
+
+        private static void DisplaySessionInfoAction(Request request, Response response)
+        {
+            var sessionExists = request.Session.ContainsKey(Session.SessionCurrentDateKey);
+
+            var bodyText = "";
+
+            if (sessionExists)
+            {
+                var currentDate = request.Session[Session.SessionCurrentDateKey];
+                bodyText = $"Stored date: {currentDate}!";
+            }
+            else
+            {
+                bodyText = "Current date stored!";
+            }
+
+            response.Body = "";
+            response.Body += bodyText;
+        }
+
+        private static void AddCookiesAction(Request request, Response response)
+        {
+            var requestHasCookies = request.Cookies.Any( c => c.Name != Session.SessionCookieName);
+            var bodyText = "";
+
+            if (requestHasCookies)
+            {
+                var cookieText = new StringBuilder();
+                cookieText.AppendLine("<h1>Cookies</h1>");
+
+                cookieText.Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
+
+                foreach (var cookie in request.Cookies)
+                {
+                    cookieText.Append("<tr>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
+                    cookieText.Append("</tr>");
+                }
+
+                cookieText.Append("</table>");
+
+                bodyText = cookieText.ToString();
+            }
+            else
+            {
+                bodyText = "<h1>Cookies set!</h1>";
+            }
+
+            if (!requestHasCookies)
+            {
+                response.Cookies.Add("My-Cookie", "My-Value");
+                response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
+            }
+
+            response.Body = "";
+            response.Body += bodyText;
         }
 
         private static void AddFormDataAction(Request request, Response response)
