279aedc045d048d39e4ca92f9f2b6cba2ba6fd5c
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2023-07-16 14:31:47 +0300

    Added cookies and session functionality

---
+++ SoftUniWebServer/SoftUniWebServer.Server/HTTP/Session.cs
@@ -0,0 +1,41 @@
+﻿using SoftUniWebServer.Server.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniWebServer.Server.HTTP
+{
+    public class Session
+    {
+        public const string SessionCookieName = "MyWebServerSID";
+        public const string SessionCurrentDateKey = "CurrentDate";
+        public const string SessionUserKey = "AuthenticatedUserId";
+
+        private Dictionary<string, string> _data;
+
+        public Session(string id)
+        {
+            Guard.AgainstNull(id, nameof(id));
+
+            Id = id;
+
+            _data = new Dictionary<string, string>();
+        }
+
+        public string Id { get; init; }
+
+        public string this[string key]
+        {
+            get => _data[key];
+            set => _data[key] = value;
+        }
+
+        public bool ContainsKey(string key)
+            => _data.ContainsKey(key);
+
+        public void Clear()
+            => _data.Clear();
+    }
+}
